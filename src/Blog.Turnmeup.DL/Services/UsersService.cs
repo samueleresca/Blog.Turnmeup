2828434c18c5950f2ac57929b283994dfa6b7e32
Samuele Resca <samuele.resca@gmail.com>
2017-03-15 00:40:36 +0100

    Setup auth service

---
+++ src/Blog.Turnmeup.DL/Services/UsersService.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Blog.Turnmeup.DAL.Models;
+using Blog.Turnmeup.DL.Repositories;
+using Microsoft.AspNetCore.Identity;
+
+namespace Blog.Turnmeup.DL.Services
+{
+    public class UsersService : IUsersService
+    {
+        private readonly IUsersRepository _repository;
+
+        public UsersService(IUsersRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IQueryable<AppUser> Get()
+        {
+            return _repository.Get();
+        }
+
+        public AppUser GetByEmail(string email)
+        {
+            return _repository.GetByEmail(email);
+        }
+
+        public Task<IdentityResult> Create(AppUser user, string password)
+        {
+            return _repository.Create(user, password);
+        }
+
+        public async Task<IdentityResult> Delete(AppUser user)
+        {
+            return await _repository.Delete(user);
+        }
+
+        public  async Task<IdentityResult> Update(AppUser user)
+        {
+            return await _repository.Update(user);
+        }
+
+        public UserManager<AppUser> GetUserManager()
+        {
+            return _repository.GetUserManager();
+        }
+
+    }
+}
---
+++ test/Blog.Turnmeup.DL.Tests/Services/UserServicesTest.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Blog.Turnmeup.DAL.Models;
+using Blog.Turnmeup.DL.Repositories;
+using Blog.Turnmeup.DL.Services;
+using Moq;
+using Xunit;
+
+namespace Blog.Turnmeup.DL.Tests.Services
+{
+    public class UserServicesTest
+    {
+
+        private Mock<IUsersRepository> Repository { get; }
+
+        private IUsersService Service { get; }
+
+        private static readonly string UniqueId = Guid.NewGuid().ToString();
+
+        public UserServicesTest()
+        {
+
+
+            var users = new List<AppUser>
+            {
+                new AppUser
+                {
+                    Id = UniqueId,
+                    UserName = "testusername",
+                    Email = "test@test.it"
+
+                }
+            };
+
+            Repository = new Mock<IUsersRepository>();
+
+            Repository.Setup(x => x.Get())
+                .Returns(users.AsQueryable());
+
+            Repository.Setup(x => x.GetByEmail(It.IsAny<string>()))
+                .Returns((string email) => users.Find(s => s.Email == email));
+
+            Repository.Setup(x => x.Create(It.IsAny<AppUser>(), It.IsAny<string>()))
+                .Callback((AppUser user, string password) => users.Add(user));
+
+            Repository.Setup(x => x.Update(It.IsAny<AppUser>()))
+                .Callback((AppUser user) => users[users.FindIndex(x => x.Id == user.Id)] = user);
+
+            Repository.Setup(x => x.Delete(It.IsAny<AppUser>()))
+            .Callback((AppUser label) => users.RemoveAt(users.FindIndex(x => x.Id == label.Id)));
+
+
+            Service = new UsersService(Repository.Object);
+        }
+
+        [Fact]
+        public void GetAllUsers()
+        {
+            // Act
+            var users = Service.Get();
+            // Assert
+            Repository.Verify(x => x.Get(), Times.Once);
+            Assert.Equal(1, users.Count());
+        }
+
+
+        [Theory]
+        [InlineData("test@test.it")]
+        public void GetSingleLabel(string userEmail)
+        {
+            // Act
+            var user = Service.GetByEmail(userEmail);
+
+            // Assert
+            Repository.Verify(x => x.GetByEmail(userEmail), Times.Once);
+            Assert.Equal(userEmail, user.Email);
+            Assert.Equal("testusername", user.UserName);
+        }
+
+
+
+        [Fact]
+        public void InsertUser()
+        {
+            // Arrange
+            var user = new AppUser
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = "testusername2",
+                Email = "test2@test.it"
+            };
+            // Act
+            Service.Create(user, "testpassword");
+            // Assert
+            Repository.Verify(x => x.Create(It.IsAny<AppUser>(), It.IsAny<string>()), Times.Once);
+            var labels = Service.Get();
+            Assert.Equal(2, labels.Count());
+        }
+
+
+        [Fact]
+        public void UpdateUser()
+        {
+            // Arrange
+            var user = new AppUser
+            {
+                Id = UniqueId,
+                UserName = "testusername",
+                Email = "test@test.it",
+                PhoneNumber = "test"
+
+            };
+            // Act
+            Service.Update(user);
+            // Assert
+            Repository.Verify(x => x.Update(It.IsAny<AppUser>()), Times.Once);
+            var labels = Service.Get();
+            Assert.Equal(1, labels.Count());
+            Assert.Equal("test", labels.First().PhoneNumber);
+        }
+
+        [Fact]
+        public void DeleteUser()
+        {
+            const string email = "test@test.it";
+            
+            // Act
+            var user = Service.GetByEmail(email);
+            Service.Delete(user);
+            
+            // Assert
+            Repository.Verify(x => x.GetByEmail(It.IsAny<string>()), Times.Once);
+            Repository.Verify(x => x.Delete(It.IsAny<AppUser>()), Times.Once);
+            Assert.Equal(0, Service.Get().Count());
+        }
+
+
+
+    }
+}
