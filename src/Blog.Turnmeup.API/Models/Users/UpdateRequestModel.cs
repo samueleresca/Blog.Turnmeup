7f5409ecd1b09d8331109836f68fad43ea396ae2
Samuele Resca <samuele.resca@gmail.com>
2017-03-24 07:29:50 +0100

    Added UpadateRequestModel

---
+++ src/Blog.Turnmeup.API/Models/Users/UpdateRequestModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Blog.Turnmeup.DL.Models;
+
+namespace Blog.Turnmeup.API.Models.Users
+{
+    public class UpdateRequestModel : UserModel
+    {
+        [Required]
+        public string Password { get; set; }
+    }
+}
